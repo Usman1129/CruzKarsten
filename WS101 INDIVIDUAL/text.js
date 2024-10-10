9acebaf725129b8794e5a5ac72eb4d7edd6ba56e
Tennsaiii <cruz.karstencyril.bala@gmail.com>
2024-10-10 23:11:16 +0800

    Delete WS101 INDIVIDUAL directory

--- WS101 INDIVIDUAL/text.js
+++
@@ -1,8 +0,0 @@
-const showImage = () => {
-    document.getElementById("container").classList.toggle('hidden');
-}
-
-const showImage2 = () => {
-    document.getElementById("container2").classList.toggle('hidden');
-}
-
