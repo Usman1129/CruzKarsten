87a8b0b04a2d8db81305b68afe374279677a5a57
Tennsaiii <cruz.karstencyril.bala@gmail.com>
2024-10-10 23:13:25 +0800

    Add files via upload

---
+++ WS101 INDIVIDUAL/text.js
@@ -0,0 +1,8 @@
+const showImage = () => {
+    document.getElementById("container").classList.toggle('hidden');
+}
+
+const showImage2 = () => {
+    document.getElementById("container2").classList.toggle('hidden');
+}
+
